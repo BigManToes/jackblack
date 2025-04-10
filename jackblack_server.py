5c868e86d6f6a0a4acafe10fecc44f52d68caadb
BigManToes <olliemoseby+github@wentworth.school.nz>
2025-04-11 09:01:59 +1200

    goon

--- jackblack_server.py
+++ jackblack_server.py
@@ -136,4 +136,5 @@ async def play_blackjack(player_name, websocket):
     await update_players()
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
